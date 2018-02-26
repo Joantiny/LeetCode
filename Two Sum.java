4223df540dfbcde8b7a24ed5ffa628fa2f0c5e53
Joantiny <33047656+Joantiny@users.noreply.github.com>
2018-02-26 15:40:36 +0800

    Two Sum
    未利用哈希表

---
+++ Two Sum.java
@@ -0,0 +1,49 @@
+class Solution {
+  public int[] twoSum(int[] nums, int target) {
+  int N=nums.length;
+	int[] indices=new int[2];
+	int[] nums_sort=new int[N];
+	for(int i=0;i<N;i++)
+	{
+		nums_sort[i]=nums[i];
+	}
+	Arrays.sort(nums);
+		for(int i=0;i<nums.length;i++)
+		{
+			for(int j=i+1; j<nums.length;j++)
+			{
+				if(nums[i]+nums[j]>target)
+				break;
+				if(nums[i]+nums[j]==target) 
+				{
+					indices[0]=i;
+					indices[1]=j;
+				}	
+			}
+				
+		}
+		for(int i=0;i<N;i++)
+		{
+			if(nums_sort[i]== nums[indices[0]])
+				{
+				indices[0]=i;
+			    break;
+				}
+			
+		}
+		for(int j=0;j<N;j++)
+		{
+            if(j==indices[0])
+			{
+			 continue;
+			}
+			if(nums_sort[j]==nums[indices[1]])
+				{
+				indices[1]=j;
+				break;
+				}
+		}
+    
+        return indices;
+    }
+}
