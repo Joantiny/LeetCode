95429f92cc8cda8cd60fb35ec710a39a293c255a
Joantiny <33047656+Joantiny@users.noreply.github.com>
2018-02-26 19:55:21 +0800

    Update 283Move Zeroes.java

--- 283Move Zeroes.java
+++ 283Move Zeroes.java
@@ -35,3 +35,18 @@ class Solution {
     }
     }
 }
+
+// 双指针定位
+public void moveZeroes(int[] nums) {
+    int leftMostZeroIndex = 0; // The index of the leftmost zero
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] != 0) {
+            if (i > leftMostZeroIndex) { // There are zero(s) on the left side of the current non-zero number, swap!
+                nums[leftMostZeroIndex] = nums[i];
+                nums[i] = 0;
+            }
+
+            leftMostZeroIndex++;//非0的元素双指针同时+1，当元素为0时，ZeroIndex指针不增长，i增长。
+        }
+    }
+}
